324a00bc944cb9e5404d2416d11e0b90de4c00a8
Ananyacodes <ananyakar2007@gmail.com>
2026-02-20 09:30:40 +0530

    UC2:Hardcoded palindrome

--- src/Palindrome_checker_app.java
+++ src/Palindrome_checker_app.java
@@ -1,5 +1,16 @@
 public class Palindrome_checker_app {
     public static void main(String args[]){
-        System.out.println("Welcome to the Palindrome Checker Managaement System\nVersion: 1.0\nSystem initialized successfully.")
+        System.out.println("Welcome to the Palindrome Checker Managaement System\nVersion: 1.0\nSystem initialized successfully.");
+        //UC2
+        String s1 = "Quetzalcoatlus is my favourite dinosaur";
+        boolean flag = true;
+        int l = s1.length();
+        for(int i=0;i<l/2;i++){
+            if(s1.charAt(i) != s1.charAt(l-i-1)){
+                flag = false;
+                break;
+            }
+        }System.out.println("Input text: "+ s1);
+        System.out.println("Is it a Palindrome? : "+flag);
     }
 }
