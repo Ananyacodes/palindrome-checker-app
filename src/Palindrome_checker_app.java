7aec92e61c3042d73b63cf46ef029a4e66311067
Ananyacodes <ananyakar2007@gmail.com>
2026-02-20 09:37:41 +0530

    UC3:Reverse check palindrome

--- src/Palindrome_checker_app.java
+++ src/Palindrome_checker_app.java
@@ -3,14 +3,13 @@ public class Palindrome_checker_app {
         System.out.println("Welcome to the Palindrome Checker Managaement System\nVersion: 1.0\nSystem initialized successfully.");
         //UC2
         String s1 = "Quetzalcoatlus is my favourite dinosaur";
-        boolean flag = true;
         int l = s1.length();
-        for(int i=0;i<l/2;i++){
-            if(s1.charAt(i) != s1.charAt(l-i-1)){
-                flag = false;
-                break;
-            }
-        }System.out.println("Input text: "+ s1);
-        System.out.println("Is it a Palindrome? : "+flag);
+        String reversed = "";
+        for(int i=l-1;i>0;i--){
+            reversed += s1.charAt(i);
+        }
+        boolean result = s1.equals(reversed);
+        System.out.println("Input text: "+ s1);
+        System.out.println("Is it a Palindrome? : "+result);
     }
 }
