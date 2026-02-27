676417bca268c43d103cca5515be29506ea9242b
Ananyacodes <ananyakar2007@gmail.com>
2026-02-27 09:11:50 +0530

    UC12: Strategy Pattern for Palindrome Algorithms

--- src/Palindrome_checker_app.java
+++ src/Palindrome_checker_app.java
@@ -1,38 +1,65 @@
 import java.util.*;
-class PalindromeChecker {
-    private Stack<Character> stack;
-    public PalindromeChecker() {
-        stack = new Stack<>();
-    }
+
+interface PalindromeStrategy {
+    boolean checkPalindrome(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
     public boolean checkPalindrome(String input) {
         String processed = input.replaceAll("\\s+", "").toLowerCase();
-
-        for (int i = 0; i < processed.length(); i++) {
-            stack.push(processed.charAt(i));
+        Stack<Character> stack = new Stack<>();
+        for (char c : processed.toCharArray()) {
+            stack.push(c);
         }
-        for (int i = 0; i < processed.length(); i++) {
-            char original = processed.charAt(i);
-            char reversed = stack.pop();
-
-            if (original != reversed) {
+        for (char c : processed.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
         }
-
         return true;
     }
 }
-public class UseCase11PalindromeCheckerApp {
 
+class DequeStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String input) {
+        String processed = input.replaceAll("\\s+", "").toLowerCase();
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : processed.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class PalindromeChecker {
+    private PalindromeStrategy strategy;
+
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String input) {
+        return strategy.checkPalindrome(input);
+    }
+}
+
+public class UseCase12PalindromeStrategyApp {
     public static void main(String args[]) {
-
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully.");
 
-        String input = "A man a plan a canal panama";
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.checkPalindrome(input);
+        String input = "madam";
+
+        PalindromeStrategy strategy = new StackStrategy();
+        PalindromeChecker checker = new PalindromeChecker(strategy);
+
+        boolean result = checker.execute(input);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
