1ee20534b1e2c610aed71532207f15f1d0b22083
Ananyacodes <ananyakar2007@gmail.com>
2026-02-27 08:42:51 +0530

    UC9: Recursive Palindrome Checker

--- src/Palindrome_checker_app.java
+++ src/Palindrome_checker_app.java
@@ -1,80 +1,25 @@
 import java.util.*;
+
 public class Palindrome_checker_app {
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public static Node createList(String s) {
-        Node head = null;
-        Node tail = null;
-
-        for (char c : s.toCharArray()) {
-            Node newNode = new Node(c);
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
-    }
-    public static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-    public static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null)
+    static int start = 0;
+    public static boolean isPalindromeRecursive(String str, int end) {
+        if (start >= end) {
             return true;
-        Node slow = head;
-        Node fast = head;
-
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
         }
-        Node secondHalf = reverse(slow.next);
-        Node firstHalf = head;
-        Node tempSecond = secondHalf;
-
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-
-        return true;
+        start++;
+        return isPalindromeRecursive(str, end - 1);
     }
-
     public static void main(String args[]) {
-        //UC8
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully.");
-
         String s1 = "madam";
-        Node head = createList(s1);
-        boolean result = isPalindrome(head);
+        start = 0;
+
+        boolean result = isPalindromeRecursive(s1, s1.length() - 1);
 
         System.out.println("Input : " + s1);
         System.out.println("Is Palindrome? : " + result);
