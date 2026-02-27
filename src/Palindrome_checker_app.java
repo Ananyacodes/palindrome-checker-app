5601156c195e3ad5b4722dd9bcd075dcfef39859
Ananyacodes <ananyakar2007@gmail.com>
2026-02-27 08:57:07 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome

--- src/Palindrome_checker_app.java
+++ src/Palindrome_checker_app.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 public class Palindrome_checker_app {
     static int start = 0;
-    public static boolean isPalindromeRecursive(String str, int end) {
-        if (start >= end) {
-            return true;
+    public static boolean isPalindromeIgnoreCase(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int n = str.length();
+        for(int i=0; i<n/2; i++){
+            if(str.charAt(i) != str.charAt(n-i-1)){
+                return false;
+            }
         }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-        start++;
-        return isPalindromeRecursive(str, end - 1);
+        return true;
     }
     public static void main(String args[]) {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully.");
-        String s1 = "madam";
+        String s1 = "A man a plan a canal panama";
         start = 0;
 
-        boolean result = isPalindromeRecursive(s1, s1.length() - 1);
+        boolean result = isPalindromeIgnoreCase(s1);
 
         System.out.println("Input : " + s1);
         System.out.println("Is Palindrome? : " + result);
